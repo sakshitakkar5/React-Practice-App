256b009cc7a6c27299af1d663a2fe491c971abbf
Sakshi <sakshitakkar5@gmail.com>
2025-12-14 20:35:36 +0530

    new commit 14 dec

--- src/ReduxExample3/ToDoSlicer.jsx
+++ src/ReduxExample3/ToDoSlicer.jsx
@@ -1,7 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todoitem : [{id:1, text:"Item1"}]
+    todoitem : JSON.parse(localStorage.getItem("todos")) || [{id:1, text:"Item1"}]
 }
 
 export const TodoSlicer = createSlice({
@@ -14,10 +14,12 @@ export const TodoSlicer = createSlice({
                 text:action.payload
             }
             
-            state.todoitem.push(newtodoitem)
+            state.todoitem.push(newtodoitem);
+            localStorage.setItem("todos", JSON.stringify(state.todoitem))
         },
         deletetodo: (state,action) =>{
-            state.todoitem =state.todoitem.filter(u=>u.id!=action.payload)
+            state.todoitem =state.todoitem.filter(u=>u.id!=action.payload);
+            localStorage.setItem("todos", JSON.stringify(state.todoitem))
         }
     }
 
